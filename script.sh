b4d7374036d32571d4d73217b31d07d914565c02
Tom Mourot-Faraut <tom.mourot-faraut7@etu.univ-lorraine.fr>
2018-02-06 17:12:01 +0100

    modif de script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-for i in $(seq 6)
+
+for j in 50 100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 1000
 do
-	for j in $(seq 100)
+	for i in $(seq 50)
 	do
-    		Python loraDirMulBSK-mean.py 100 1002000 1 5011200000 $i
+    		Python loraDirMulBSK-mean.py $j 1002000 1 5011200000 6
+		Python loraDirMulBSK-medoids.py $j 1002000 1 5011200000 6
+		Python loraDirMulBSMean-shifts.py $j 1002000 1 5011200000 
+		Python loraDirMulBSFixe.py $j 1002000 1 5011200000 6
 	done
 done
-
 
