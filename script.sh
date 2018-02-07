eed2c4fc32a62ad70e843a20827d2ababa379f4d
Tom Mourot-Faraut <tom.mourot-faraut7@etu.univ-lorraine.fr>
2018-02-07 19:51:07 +0100

    Script

--- script.sh
+++ script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for j in 50 100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 1000
+for i in $(seq 31)
+do
+	Python loraDirMulBSK-mean.py 100 1002000 1 5011200000 6
+	Python loraDirMulBSK-medoids.py 100 1002000 1 5011200000 6
+	Python loraDirMulBSMean-shifts.py 100 1002000 1 5011200000 
+	Python loraDirMulBSFixe.py 100 1002000 1 5011200000 6
+done
+
+for j in 150 200 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 1000
 do
 	for i in $(seq 50)
 	do
